7987231ca5f8e2377dfb53383a80c55ba8719a62
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-20 23:31:59 +0700

    Initial Commit

---
+++ little_lemon_reservation_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  little_lemon_reservation_app
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 20/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ little_lemon_reservation_app/little_lemon_reservation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_lemon_reservation_appApp.swift
+//  little_lemon_reservation_app
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct little_lemon_reservation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
